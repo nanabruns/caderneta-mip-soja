e7733f31afa0f21f47aa5212f37d1360707fff74
Natacha <nanabruns@gmail.com>
2025-10-27 16:57:24 -0400

    Atualiza modelo do Gemini para 1.5-flash

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (!apiKey) {
 }
 
 const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model:'gemini-pro' });
+const model = genAI.getGenerativeModel({ model:'gemini-1.5-flash' });
 
 app.post("/analisar", async (req, res) => {
   try {
